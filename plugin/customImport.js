d48abeeb7fe6590895a78704d8618085d4104f4a
danielabdelsamed <daniel@abdelsamed.com>
2018-11-30 17:34:53 -0500

    Added ability to import files that are known to be unique

--- plugin/customImport.js
+++ plugin/customImport.js
@@ -1,12 +1,36 @@
-import { readFileSync } from 'fs'
+import { readFileSync, readdirSync, statSync } from 'fs'
+import path from 'path'
 
 import { newlinePattern } from './constants'
+
+const walk = function(dir, targetFilename) {
+  let results = []
+  const list = readdirSync(dir)
+  list.forEach(function(file) {
+    file = path.join(dir, file)
+    const stat = statSync(file)
+    if (stat && stat.isDirectory()) {
+      /* Recurse into a subdirectory */
+      results = results.concat(walk(file, targetFilename))
+    } else if (path.parse(file).name === targetFilename) {
+      /* Is a file */
+      results.push(file)
+    }
+  })
+  return results
+}
 
 export function getFilepaths(src, relFile, resolve) {
   const imports = src.split(newlinePattern).filter(line => line.startsWith('#import'))
   return imports.map(statement => {
-    const importPath = statement.split(/[\s\n]+/g)[1].slice(1, -1)
-    return resolve(importPath, relFile)
+    if (statement.startsWith('#importu')) {
+      const filename = statement.split(/[\s\n]+/g)[1]
+      const importPath = walk('.', filename)[0]
+      return resolve(importPath, '.')
+    } else {
+      const importPath = statement.split(/[\s\n]+/g)[1].slice(1, -1)
+      return resolve(importPath, relFile)
+    }
   })
 }
 
